57a68ba59b1f59dd5c9c57163aab654ae06c5eb7
RenatoGarciaLopes <renatovideos41@gmail.com>
2025-06-21 00:28:42 -0400

    tirando warning pro deploy

--- src/shared/services/collaboratorService.ts
+++ src/shared/services/collaboratorService.ts
@@ -1,4 +1,4 @@
-import { collection, getDocs, addDoc, doc, updateDoc, deleteDoc } from 'firebase/firestore';
+import { collection, getDocs, addDoc } from 'firebase/firestore';
 import { db } from './firebase.ts';
 import type { Funcionario } from '../types/funcionario.ts';
 
